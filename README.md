077830262447c2fba740b4c2d499bf843076668b
MikeSteel <88979808+Maikito99XD@users.noreply.github.com>
2022-02-16 19:27:00 +0100

    a
    a

--- README.md
+++
@@ -1,2 +0,0 @@
-# MusiTeca3
- 
--- src/main/java/es/multitiendaMike/modelo/dao/IPlayListDao.java
+++
@@ -1,9 +0,0 @@
-package es.multitiendaMike.modelo.dao;
-
-import java.util.List;
-
-import es.multitiendaMike.modelo.entidades.Playlist;
-
-public interface IPlayListDao {
-public List<Playlist> findAll();
-}
