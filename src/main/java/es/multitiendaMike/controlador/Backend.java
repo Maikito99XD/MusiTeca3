d44df5a5a7d520bb61fefc66d86e73eaaf9763d6
MikeSteel <88979808+Maikito99XD@users.noreply.github.com>
2022-02-17 19:45:37 +0100

    act

--- src/main/java/es/multitiendaMike/controlador/Backend.java
+++ src/main/java/es/multitiendaMike/controlador/Backend.java
@@ -4,13 +4,18 @@ import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import es.multitiendaMike.modelo.servicios.CancionService;
+
 @Controller
 public class Backend {
+	@Autowired
+	private CancionService cancionService;
 	@GetMapping({"/admin"})
 	public String admin(Model model) throws MalformedURLException {
 		
@@ -23,5 +28,12 @@ public class Backend {
 		model.addAttribute("host",requestURL.getHost());
 		
 		return "backend/admin";
+	}
+	
+	@GetMapping({"/admin/listadoCanciones"})
+	public String listadoCanciones(Model model) throws MalformedURLException{
+		
+		//model.addAttribute("listarCanciones",cancionService.findAll());
+		return "backend/listadoCanciones";
 	}
 }
