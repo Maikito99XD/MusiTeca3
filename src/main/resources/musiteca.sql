e8962fdcf3f4a9d3b5c15bdc1a43212f958392b5
MikeSteel <88979808+Maikito99XD@users.noreply.github.com>
2022-02-19 21:04:46 +0100

    MusiTecaActualizadaYFuncional

--- src/main/resources/musiteca.sql
+++ src/main/resources/musiteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-02-2022 a las 16:27:45
+-- Tiempo de generación: 19-02-2022 a las 20:48:17
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -29,11 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cancion` (
   `id` bigint(20) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `enlaceCancion` varchar(10000) NOT NULL,
-  `fk_playlist` int(11) NOT NULL,
-  `fk_genero` int(11) NOT NULL
+  `enlace_cancion` varchar(255) DEFAULT NULL,
+  `fk_genero` bigint(20) DEFAULT NULL,
+  `fk_playlist` bigint(20) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cancion`
+--
+
+INSERT INTO `cancion` (`id`, `enlace_cancion`, `fk_genero`, `fk_playlist`, `nombre`) VALUES
+(1, 'enlace cancion', 1, 1, 'Cancion de rock'),
+(2, 'enlace cancion 2', 2, 2, 'Cancion de pop'),
+(3, 'enlace cancion 3', 3, 3, 'Cancion de reggeton'),
+(4, 'enlace cancion 4', 1, 1, 'Otra cancion de rock');
 
 -- --------------------------------------------------------
 
@@ -43,8 +53,34 @@ CREATE TABLE `cancion` (
 
 CREATE TABLE `genero` (
   `id` bigint(20) NOT NULL,
-  `nombre` varchar(100) NOT NULL
+  `nombre` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `genero`
+--
+
+INSERT INTO `genero` (`id`, `nombre`) VALUES
+(1, 'Rock'),
+(2, 'Pop'),
+(3, 'reggeton');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `hibernate_sequence`
+--
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `hibernate_sequence`
+--
+
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+(1);
 
 -- --------------------------------------------------------
 
@@ -54,9 +90,18 @@ CREATE TABLE `genero` (
 
 CREATE TABLE `playlist` (
   `id` bigint(20) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
   `valoracion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `playlist`
+--
+
+INSERT INTO `playlist` (`id`, `nombre`, `valoracion`) VALUES
+(1, 'Canciones de pop', 2),
+(2, 'Canciones de michael jackson', 4),
+(3, 'Bandas de rock que me gustan', 5);
 
 --
 -- Índices para tablas volcadas
@@ -66,7 +111,9 @@ CREATE TABLE `playlist` (
 -- Indices de la tabla `cancion`
 --
 ALTER TABLE `cancion`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_genero` (`fk_genero`),
+  ADD KEY `fk_playlist` (`fk_playlist`);
 
 --
 -- Indices de la tabla `genero`
@@ -81,26 +128,15 @@ ALTER TABLE `playlist`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Restricciones para tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `cancion`
+-- Filtros para la tabla `cancion`
 --
 ALTER TABLE `cancion`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `genero`
---
-ALTER TABLE `genero`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `playlist`
---
-ALTER TABLE `playlist`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  ADD CONSTRAINT `cancion_ibfk_1` FOREIGN KEY (`fk_genero`) REFERENCES `genero` (`id`),
+  ADD CONSTRAINT `cancion_ibfk_2` FOREIGN KEY (`fk_playlist`) REFERENCES `playlist` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
